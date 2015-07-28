3c6aff23fb735d889513991a3493ddb18a7bcbc8
yianyue <yianyue@gmail.com>
2015-07-28 13:51:11 -0400

    name space Admin

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,26 @@
+module Admin
+  class UsersController < ApplicationController
+  
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to movies_path
+      else
+        render :new
+      end
+    end
+
+    protected
+
+    def user_params
+      params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
+    end
+
+  end
+
+end

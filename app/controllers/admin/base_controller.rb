4e76368c944366159cdcbfb680eb37777f1b0035
yianyue <yianyue@gmail.com>
2015-07-30 15:29:44 -0400

    fixed users/_form

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,5 @@
 class Admin::BaseController < ApplicationController
+  
   before_action :admin_access 
+  
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,64 +1,59 @@
-module Admin
-  class UsersController < Admin::BaseController
-    # what about inhereting from UsersController? Wouldn't be super useful, since only new is the same
+class Admin::UsersController < Admin::BaseController
+  # what about inhereting from UsersController? Wouldn't be super useful, since only new is the same
 
-
-    def index
-      @users = User.page(params[:page]).per(10)
-    end
-  
-    def new
-      @user = User.new
-    end
-
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        redirect_to admin_users_path
-      else
-        render :new
-      end
-    end
-
-    def show
-      @user = User.find(params[:id])
-    end
-
-    def edit
-      @user = User.find(params[:id])
-    end
-
-    def update
-      @user = User.find(params[:id])
-      if @user.update_attributes(user_params)
-        redirect_to admin_user_path(@user), notice: "User info updated!"
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @user = User.find(params[:id])
-      # Tell the UserMailer to send a welcome email after save
-      UserMailer.delete_email(@user).deliver_later
-      @user.destroy
-      redirect_to admin_users_path
-    end
-
-  # is this the right place to put become_user?
-  # TODO: create Admin::BaseController
-
-    def become_user
-      # return unless current_user.admin
-      session[:mock_id] = params[:id]
-      redirect_to root_url
-    end
-
-    protected
-
-    def user_params
-      params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation, :admin)
-    end
-
+  def index
+    @users = User.page(params[:page]).per(10)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user), notice: "User info updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    # Tell the UserMailer to send a welcome email after save
+    UserMailer.delete_email(@user).deliver_later
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+# is this the right place to put become_user?
+
+def become_user
+  session[:mock_id] = params[:id]
+  redirect_to root_url
+end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation, :admin)
+  end
+
 end

9a955c281da29a638facc267a499f0bf65e34773
yianyue <yianyue@gmail.com>
2015-07-30 20:36:23 -0400

    no longer N+1. TODO: prevent execption when id not found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @reviews = @user.reviews.includes(:movie)
+    # breaks when id not found
   end
 
   protected
